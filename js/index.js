ceca6a2a8f7fb9a62c192b183a08e79089d5a264
ElenaPrygun <66680726+ElenaPrygun@users.noreply.github.com>
2022-10-20 12:03:11 +0100

    ls_5_pagination for Latest Blog (#8)
    * ls_5_pagination for Latest Blog
    
    * ls_5_optimize
    
    * ls_5_fixed

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// Mobile menu
+import "./mobile-menu.js";
+
+// Latest Blog
+import "./paginator.js";
+import { paginator, dataForLatestBlog } from "./paginator.js";
+
+paginator("#paginator", dataForLatestBlog);
