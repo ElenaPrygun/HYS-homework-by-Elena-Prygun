be8b1e6abb20470ed235c589fc3cda7e452da1a9
Elena Prygun <elena.prygun@gmail.com>
2022-10-21 09:21:37 +0100

    ls_6_class-slider

--- js/index.js
+++ js/index.js
@@ -7,9 +7,42 @@ import { paginator, dataForLatestBlog } from "./paginator.js";
 
 paginator("#paginator", dataForLatestBlog);
 
-// Which one do you prefer, 2000+ Online courses
+// Which one do you prefer
+import "./slider.js";
+import { Slider } from "./slider.js";
+import { dataForSlider } from "./data.js";
+
+const slider = new Slider("#slider");
+slider.setData(dataForSlider);
+
+// 2000+ Online courses- slick-slider
 import "https://code.jquery.com/jquery-3.6.1.min.js";
 import "./slick.min.js";
-import "./slider.js";
 
+$(document).ready(function () {
+  $(".avaliable-courses").slick({
+    mobileFirst: true,
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    adaptiveHeight: true,
 
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
